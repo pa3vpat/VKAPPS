8b65553520821de529cfe7380cc4d0e16976027b
pa3vpat <urkytsk.pro@gmail.com>
2020-02-23 19:53:05 +0300

    mur

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenS
 import '@vkontakte/vkui/dist/vkui.css';
 
 import Home from './panels/Home';
-import Persik from './panels/Persik';
+import mur from './panels/mur';
 
 const App = () => {
 	const [activePanel, setActivePanel] = useState('home');
@@ -35,7 +35,7 @@ const App = () => {
 	return (
 		<View activePanel={activePanel} popout={popout}>
 			<Home id='home' fetchedUser={fetchedUser} go={go} />
-			<Persik id='persik' go={go} />
+			<mur id='mur' go={go} />
 		</View>
 	);
 }
