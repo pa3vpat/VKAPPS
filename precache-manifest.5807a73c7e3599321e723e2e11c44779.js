5b0c074feb1aba9b3567d8e7a2f93d233c845d80
pa3vpat <urkytsk.pro@gmail.com>
2020-02-23 15:02:11 +0300

    Updates

---
+++ precache-manifest.5807a73c7e3599321e723e2e11c44779.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e2cc4e392814248c0d203cf093a6862",
+    "url": "/VKAPPS/index.html"
+  },
+  {
+    "revision": "8524aae5638ef5a27aca",
+    "url": "/VKAPPS/static/css/2.ae2f4c93.chunk.css"
+  },
+  {
+    "revision": "e71535464fd968a438ee",
+    "url": "/VKAPPS/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "8524aae5638ef5a27aca",
+    "url": "/VKAPPS/static/js/2.bfdf1492.chunk.js"
+  },
+  {
+    "revision": "e71535464fd968a438ee",
+    "url": "/VKAPPS/static/js/main.7c0edde3.chunk.js"
+  },
+  {
+    "revision": "15b097e2e522fe3c2705",
+    "url": "/VKAPPS/static/js/runtime-main.f7aa3858.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/VKAPPS/static/media/persik.4e1ec840.png"
+  }
+]);
